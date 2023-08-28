e919f99f90e3cca394ab184550186d4b7ce1c62d
rado <hei.rado.2@gmail.com>
2023-08-28 16:53:52 +0300

    fix: add endpoint post

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
 import Profile from './pages/profile/Profile';
+import Post from './pages/Post/post'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
              <Route path="/" element={<Register />} />
               <Route path="/home" element={<Home />} />
               <Route path="/user" element={<Profile />} />
+              <Route path="/posts" element={<Post />} />
     </Routes>
   </BrowserRouter>
   );
